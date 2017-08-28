caf2aa28552b562aa9a3bcc9ade9d3eb874012c1
Takayuki <Takayuki@192.168.3.11>
2017-08-29 00:19:27 +0900

    HelloWorldTest

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main (String[] args) {
+		int x;
+		x = 5;
+		String y;
+		y = "HelloWorld";
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
